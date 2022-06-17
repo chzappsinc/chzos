c8b0c176931fa242449d4829a535d8c8118a2d9f
CHZAPPS INDIA <65512279+chzappsinc@users.noreply.github.com>
2022-06-18 01:47:29 +0530

    Update getData.js

--- getData.js
+++ getData.js
@@ -1,7 +1,14 @@
 import chzos from "../../../../../../chzos/req"
 
 const getData = ({q}) => {
-  return chzos.sshQuery(q)
+  query = q ? q : "get error as error_no_query from .hidden_error_ssh"
+  chzos.sshQuery(q).then((data) => {
+  return data;
+  })
+  .catch((error)=>{
+    chzos.setTmp("error_enq:" + error)
+    return {"error" : error}
+  })
 }
 
 /**
