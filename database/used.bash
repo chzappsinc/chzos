06ba57173f8c5ac32cb29737893c3fe4ef064756
CHZAPPS INDIA <65512279+chzappsinc@users.noreply.github.com>
2022-06-18 01:33:49 +0530

    Add files via upload

---
+++ database/used.bash
@@ -0,0 +1,12 @@
+$start->users
+#"database":"key":"data"
+#"users":"xyz":"{name : "bash", class:1}"
+#"users":"8AS":"{name : "kadar", class:1}"
+#"users":"8MS":"{name : "qutin", class:1}"
+$end->users
+<!-- current session database -->
+$start->currentSession
+#"database":"key":"data"
+#"currentSession":"chrome":"xyzs"
+#"currentSession":"firefox":"xyzs"
+$end->currentSession
---
+++ database/used.encyp.bash
@@ -0,0 +1,12 @@
+૳b��`n��f��
+%{eb�bcb�fzsf�zeb�b
+%{��f��z���z�vbuf@z@cb�ik@dtb��z��
+%{��f��z���z�vbuf@z@sbeb�k@dtb��z��
+%{��f��z���z�vbuf@z@���qvk@dtb��z��
+%�fve`n��f��
+%LZ``@d���fv�@�f��qwv@eb�bcb�f@``n
+%૳b��`nd���fv��f��qwv
+%{eb�bcb�fzsf�zeb�b
+%{d���fv��f��qwvzdi�wufz����
+%{d���fv��f��qwvzgq�fgw�z����
+%�fve`nd���fv��f��qwv
