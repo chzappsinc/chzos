7fd2dde7075cc6a247d17eca995733d8d871370e
jasimchz <144520796+jasimchz@users.noreply.github.com>
2024-11-20 16:51:16 +0400

    Update getData.js

--- getData.js
+++ getData.js
@@ -1,14 +1,32 @@
-import chzos from "../../../../../../chzos/req"
+import chzos from "@hybeck/chzOS"
+import loadBalancer from "chzos/config"
+const osVersion = chzos.versions.V5
 
-const getData = ({q}) => {
-  query = q ? q : "get error as error_no_query from .hidden_error_ssh"
-  chzos.sshQuery(q).then((data) => {
+const getData = (query:chzDatabase) => {
+
+  const initQuery = query.init({
+    osVersion,
+    jsEnbaled : true,
+    database : "mongo_8.0",
+    location : "ap-south-1",
+    extended : true, //false to avoid future billings and its will not overlap if you set billing = false in config
+    provider : "hybeck"
+  })
+
+  chz.sshQuery(query?.query)
+  .then((data) => {
   return data;
   })
   .catch((error)=>{
-    chzos.setTmp("error_enq:" + error)
-    return {"error" : error}
+    chzos.error("db_error:" + error)
+    return chz.utils.toJSON({
+      error,
+      errorMessage : error?.message,
+      osVersion,
+      beautify:true
+     })
   })
+  return query?.LoadMS(loadBalancer?.maxMS || 90000 ) ? query?.exit() : query?.exec()
 }
 
 /**
